d1b39568e623e97110cbbae7caffd8e1ba8238da
eboomyl <e_boom@qq.com>
2015-08-20 01:51:02 +0800

    搞定返回json格式的数据

--- src/main/java/com/faceooo/nian/dao/UserDAO.java
+++ src/main/java/com/faceooo/nian/dao/UserDAO.java
@@ -13,16 +13,16 @@ import java.util.List;
 @Repository("userDAO")
 public interface UserDAO {
 
-    public UserinfoDTO queryUserInfoForID(String userid) throws SQLException ;
+    UserinfoDTO queryUserInfoForID(String userid) throws SQLException ;
 
-    public List<UserinfoDTO> queryTimelineUsers(String souvenirid)throws SQLException;
+    List<UserinfoDTO> queryTimelineUsers(String souvenirid)throws SQLException;
 
-    public List<String> querySoureviewList(String souvenirid)throws SQLException;
+    List<String> querySoureviewList(String souvenirid)throws SQLException;
 
-    public List<SouvenirtypeDTO> queryUserSouTypeList(UserinfoDTO userinfo)throws SQLException;
+    List<SouvenirtypeDTO> queryUserSouTypeList(UserinfoDTO userinfo)throws SQLException;
 
-    public void insertUserinfo(UserinfoDTO userinfo)throws SQLException;
+    void insertUserinfo(UserinfoDTO userinfo)throws SQLException;
 
-    public UserinfoDTO queryUserLogin(UserinfoDTO userinfo)throws SQLException  ;
+    UserinfoDTO queryUserLogin(UserinfoDTO userinfo)throws SQLException  ;
 
 }
